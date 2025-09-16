d720950b5e0c246e728fee18404e666aef74f2c0
Deepesh70 <deepeshdangi700@gmail.com>
2025-09-16 20:33:34 +0530

    princing changes

--- BlackBook/src/components/Common/Footer.jsx
+++ BlackBook/src/components/Common/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <footer className="bg-black py-6 text-center text-sm text-zinc-400">
+      <footer className="bg-zinc-900 py-6 text-center text-sm text-zinc-400">
         © {new Date().getFullYear()} BlackBookEDU.ai. All rights reserved.
       </footer>
     </div>
