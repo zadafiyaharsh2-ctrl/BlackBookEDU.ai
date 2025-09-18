87757e34505460978ba42a0ae2e9ba4b56d2425c
Deepesh70 <deepeshdangi700@gmail.com>
2025-09-18 23:10:04 +0530

    Basic contact page

--- BlackBook/src/pages/Contact.jsx
+++ BlackBook/src/pages/Contact.jsx
@@ -6,8 +6,29 @@ const Contact = () => {
   return (
     <>
       <Navbar />
-      <div className='min-h-screen w-full bg-black text-white flex justify-center items-center'>
-        <h1>Contact Us</h1>
+      <div className='min-h-screen w-full bg-white text-black flex justify-center items-center'>
+        <div className='flex flex-col justify-center items-center space-y-4'>
+        <h1 className='text-4xl font-bold'>Contact Us</h1>
+        <p className='text-center'>We would love to hear from you! Please fill out the form below.</p>
+        <div className='flex justify-center top-4 left-4'>
+          <form
+            action=""
+            className='flex flex-col justify-center items-center'
+            onSubmit={(e) => {
+              e.preventDefault();
+              alert("Form submitted!");
+            }}
+          >
+            <input type="text" name="Firstname" placeholder="Your First Name" className='border p-2 m-2' />
+            <input type="text" name="Lastname" placeholder="Your Last Name" className='border p-2 m-2' />
+            <input type="email" name="email" placeholder="Your Email" className='border p-2 m-2' />
+            <input type="phone" name="phone" placeholder="Your Phone Number" className='border p-2 m-2' />
+            <textarea name="message" placeholder="Your Message" className='resize-none border p-2 m-2' ></textarea>
+            <button type="submit" className='bg-blue-500 text-white p-2 m-2'>Send</button>
+          </form>
+            </div>
+
+        </div>
       </div>
       <Footer />
     </>
