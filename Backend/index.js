a465c500a5bb647466086428dfe381c8e02ba566
Deepesh70 <deepeshdangi700@gmail.com>
2025-10-08 18:06:23 +0530

    added new route - user.js basic profile rendered from backdend with mongo db storage

--- Backend/index.js
+++ Backend/index.js
@@ -18,14 +18,14 @@ const csrf = require("csurf");
 // app.use(express.json());
 
 // +======================== IMPORT OF MODELS ==========================
-const User = require('./models/User');
+// const User = require('./models/User');
 
 
 // ========================== IMPORT OF ROUTES =========================
 const authRoutes = require('./routes/auth');
-
+const userRoutes = require('./routes/user');
 // ========================== IMPORT OF CONTACT ROUTES =========================
-const contactRoutes = require('./routes/contact');
+// const contactRoutes = require('./routes/contact');
 
 
 ///=============================== DATABASE =============================
@@ -65,7 +65,6 @@ app.use(express.json());
 app.use(express.urlencoded( { extended:true } ));
 app.use(cookieParser());
 
-
 app.use(helmet());
 
 // Rate Limiting , only for admin/ routes
@@ -97,6 +96,7 @@ app.use(passport.initialize());
 ///  ======================= ROUTES =====================
 
 app.get( "/" , (req, res) => res.send( " Can you see me?" ) );
-app.use(authRoutes);
-app.use('/contact', contactRoutes);
+// app.use('/auth', authRoutes);
+app.use('/api/user', userRoutes);
+// app.use('/contact', contactRoutes);
 app.listen( port , () => console.log("App is running on port http://localhost:" + port));
