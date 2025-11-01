6e4f01fad85e611a63ed5acdcdb007ee0796dabc
Deepesh70 <deepeshdangi700@gmail.com>
2025-11-01 20:30:41 +0530

    problem model updated with user having options  to select and answer checked on backend

--- Backend/routes/problems.js
+++ Backend/routes/problems.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Problem = require('../models/Problem');
-
+const mongoose = require('mongoose');
 const { authenticate } = require('../utils/authentication');
 
 
 
 router.get("/", authenticate, async (req, res) => {
   try{
-      const problems = await Problem.find({});
+      const problems = await Problem.find({}).select('-answer');
 
       if(!problems){
         return res.status(404).json({ message: "No problems found" });
@@ -21,4 +21,36 @@ router.get("/", authenticate, async (req, res) => {
   }
 
 });
+
+
+router.post("/check", authenticate, async (req, res) => {
+  const { problemId , userAnswer } = req.body;
+
+  if(!problemId || !userAnswer){
+    return  res.status(400).json({ message: "problemId and userAnswer are required" });
+  }
+  try{
+    const problem = await Problem.findById(problemId).select('answer');
+
+  if(!problem){
+    return res.status(404).json({ message: "Problem not found" });
+  }
+  // CHECK THE ANSWER
+  const isCorrect = problem.answer === userAnswer;
+
+
+  //SEND BACK ONLY THE RESULT AND THE CORRECT ANSWER
+  res.json(
+    {
+      isCorrect: isCorrect,
+      correctAnswer: problem.answer   //Only sent after the user attempts the question
+    }
+  );
+
+  } catch (error) {
+    console.error("Error checking answer:", error);
+    res.status(500).json({ message: "Server error while checking answer" });
+  }
+})
+
 module.exports = router;
